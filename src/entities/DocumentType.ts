be69c8355614920ec3d31cba6126098fba37be52
Liseth Castillo <lisethcastillo.0325@gmail.com>
2020-04-18 17:56:10 -0500

    Primer ajuste modulo cliente

---
+++ src/entities/DocumentType.ts
@@ -0,0 +1,30 @@
+import { Entity, Column, PrimaryGeneratedColumn, Unique, CreateDateColumn, UpdateDateColumn  } from 'typeorm';
+
+@Entity()
+export class DocumentType {
+
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({
+        type: "varchar",
+        length: 50,
+        unique: true
+    })
+    name: string;
+
+    @Column()
+    modulo: number;
+
+    @Column()
+    estado: number;
+
+    @Column()
+    @CreateDateColumn()
+    createdat: Date;
+
+    @Column()
+    @UpdateDateColumn()
+    updatedat: Date;
+
+}
